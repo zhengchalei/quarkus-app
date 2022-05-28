e6a1cb6f96d43393c9592a620a6aeab51c73a250
小石头 <31665079+zhengchalei@users.noreply.github.com>
2022-05-28 18:23:43 +0000

    fix test

--- src/native-test/java/io/github/zhengchalei/ExampleResourceIT.java
+++
@@ -1,9 +0,0 @@
-package io.github.zhengchalei;
-
-import io.quarkus.test.junit.QuarkusIntegrationTest;
-
-@QuarkusIntegrationTest
-public class ExampleResourceIT extends ExampleResourceTest {
-
-    // Execute the same tests but in native mode.
-}
